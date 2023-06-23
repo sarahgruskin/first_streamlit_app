697381805c07413329f79fd79f3eb6abe54ace4a
Sarah Gruskin <113390007+sarahgruskin@users.noreply.github.com>
2023-06-23 10:59:46 -0400

    Update streamlit_app.py
    added table to smoothie section

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as sl
+import pandas as pd
 
 sl.title("My Parents New Healty Diner")
 
@@ -7,5 +8,8 @@ sl.text("🥣 Omega 3 & Blueberry Oatmeal")
 sl.text("🥗 Kale, Spinach & Rocket Smoothie")
 sl.text("🐔 Hard-Boiled Free-Range Egg")
 sl.text("🥑🍞 Avocado Toast")
-
 sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv(""https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+sl.dataframe(my_fruit_list)
+
