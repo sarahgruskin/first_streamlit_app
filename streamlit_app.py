f701ad0025bbb6c674bccbf998511bb3d54febd1
Sarah Gruskin <113390007+sarahgruskin@users.noreply.github.com>
2023-06-23 10:55:53 -0400

    Update streamlit_app.py
    changed title
    added another menu item
    added emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 import streamlit as sl
 
-sl.title("My Parents New Healthy Diner")
+sl.title("Breakfast Favorites")
 
 sl.header("Breakfast Menu")
-sl.text("Omega 3 & Blueberry Oatmeal")
-sl.text("Kale, Spinach & Rocket Smoothie")
-sl.text("Hard-Boiled Free-Range Egg")
+sl.text("🥣 Omega 3 & Blueberry Oatmeal")
+sl.text("🥗 Kale, Spinach & Rocket Smoothie")
+sl.text("🐔 Hard-Boiled Free-Range Egg")
+sl.text("🥑🍞 Avocado Toast")
