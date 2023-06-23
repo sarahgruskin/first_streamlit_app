1bf1d16acb9141ed6ae4f58f7914ba4b6ffa2789
Sarah Gruskin <113390007+sarahgruskin@users.noreply.github.com>
2023-06-23 11:00:14 -0400

    Update streamlit_app.py
    fix accidental multi quote

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,6 +10,6 @@ sl.text("🐔 Hard-Boiled Free-Range Egg")
 sl.text("🥑🍞 Avocado Toast")
 sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
-my_fruit_list = pandas.read_csv(""https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 sl.dataframe(my_fruit_list)
 
