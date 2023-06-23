a2d08f357dbaab7bbebe3cb9505f8f94be3f712b
Sarah Gruskin <113390007+sarahgruskin@users.noreply.github.com>
2023-06-23 10:57:41 -0400

    Update streamlit_app.py
    added back correct title
    added smoothie header

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,11 @@
 import streamlit as sl
 
-sl.title("Breakfast Favorites")
+sl.title("My Parents New Healty Diner")
 
-sl.header("Breakfast Menu")
+sl.header("Breakfast Favorites")
 sl.text("🥣 Omega 3 & Blueberry Oatmeal")
 sl.text("🥗 Kale, Spinach & Rocket Smoothie")
 sl.text("🐔 Hard-Boiled Free-Range Egg")
 sl.text("🥑🍞 Avocado Toast")
+
+sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
