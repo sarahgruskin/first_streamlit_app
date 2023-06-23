cb7b054a3f0b73fc1a94ab1f1fc1b480ee618485
Sarah Gruskin <113390007+sarahgruskin@users.noreply.github.com>
2023-06-23 11:01:51 -0400

    Update streamlit_app.py
    fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as sl
-import pandas as pd
 
 sl.title("My Parents New Healty Diner")
 
@@ -8,8 +7,10 @@ sl.text("🥣 Omega 3 & Blueberry Oatmeal")
 sl.text("🥗 Kale, Spinach & Rocket Smoothie")
 sl.text("🐔 Hard-Boiled Free-Range Egg")
 sl.text("🥑🍞 Avocado Toast")
+
 sl.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-sl.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
 
