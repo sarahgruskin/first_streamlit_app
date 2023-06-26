ab0bcebcc2294a171a0802fe1e26905804dbf829
Sarah Gruskin <113390007+sarahgruskin@users.noreply.github.com>
2023-06-26 11:21:54 -0400

    Update streamlit_app.py
    fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,4 +24,4 @@ sl.dataframe(fruits_to_show)
 #New Section to display fruityvice api response
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+sl.text(fruityvice_response)
