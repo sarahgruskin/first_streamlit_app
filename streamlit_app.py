3c5ac9c94e1df7e9b8d7fcdf7143e5a5e128bfba
Sarah Gruskin <113390007+sarahgruskin@users.noreply.github.com>
2023-06-26 12:54:41 -0400

    Update streamlit_app.py
    add a try and except to requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,15 +26,19 @@ sl.dataframe(fruits_to_show)
 
 #New Section to display fruityvice api response
 sl.header("Fruityvice Fruit Advice!")
-fruit_choice = sl.text_input('What fruit would you like information about?','Kiwi')
-sl.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = sl.text_input('What fruit would you like information about?')
+if not fruit_choice:
+  sl.error("Please select a fruit to get information")
+else:
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  sl.dataframe(fruityvice_normalized)
+except URLError as e:
+sl.error()
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
-# write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
-sl.dataframe(fruityvice_normalized)
+
 
 my_cnx = snowflake.connector.connect(**sl.secrets["snowflake"])
 my_cur = my_cnx.cursor()
