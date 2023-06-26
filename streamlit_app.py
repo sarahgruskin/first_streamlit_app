6e637fade7384ac53188fb0dfad853039d802949
Sarah Gruskin <113390007+sarahgruskin@users.noreply.github.com>
2023-06-26 12:55:19 -0400

    Update streamlit_app.py
    fix typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,12 +28,12 @@ sl.dataframe(fruits_to_show)
 sl.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = sl.text_input('What fruit would you like information about?')
-if not fruit_choice:
-  sl.error("Please select a fruit to get information")
-else:
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-  sl.dataframe(fruityvice_normalized)
+  if not fruit_choice:
+    sl.error("Please select a fruit to get information")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    sl.dataframe(fruityvice_normalized)
 except URLError as e:
 sl.error()
 
