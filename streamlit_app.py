54edd773d1b9145eb238d6db21c0dde1eb3f2590
Sarah Gruskin <113390007+sarahgruskin@users.noreply.github.com>
2023-06-26 12:45:56 -0400

    Update streamlit_app.py
    add second box for user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,7 @@ my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 sl.header("The fruit load list contains")
 sl.dataframe(my_data_rows)
+
+# Allow the end user to add a fruit to the list
+add_my_fruit = sl.text_input('What fruit would you like to add?','Kiwi')
+s1.write(f'Thanks for adding {add_my_fruit})
